8b2d9618ac1cfa962755f2d8c97ddf8e725b2524
prakp <prak.p@netobjex.com>
2023-10-08 17:12:34 +0700

    Env variables

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`<h2>Welcome to our application!!!!!</h2>`);
+  res.send(`
+  <h1>Welcome to our application!!!!!</h1>
+  <h2>Are you ready?</h2>
+  <link>http://localhost:${port}</link>
+  `);
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`));
